160ada899ff7f34c0ceb4b6727ef9fd376d7994e
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2025-07-26 10:01:17 +0530

    added docs and postman collection

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const { registerUser, loginUser } = require('./controllers/authController');
 const { addProduct, updateQuantity, getProducts } = require('./controllers/productController');
 const { authenticateToken } = require('./middlewares/authMiddleware');
 require('dotenv').config();
 
+
 const app = express();
 connectDB();
+
+
+const swaggerDocument = YAML.load('./openapi.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.use(cors());
 app.use(express.json());
